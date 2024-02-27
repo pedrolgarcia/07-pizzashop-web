01dfa5722f568200f3e6c0fe2576e02b460b5881
Pedro Lopes <pedrolopesgrc@gmail.com>
2024-02-26 21:12:39 -0300

    feat: router dom and helmet title changes

--- src/pages/app/dashboard.tsx
+++ src/pages/app/dashboard.tsx
@@ -1,9 +1,9 @@
-import { Button } from '@/components/ui/button'
+import { Helmet } from 'react-helmet-async'
 
 export function Dashboard() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <Button>Dashboard</Button>
-    </div>
+    <>
+      <Helmet title="Dashboard" />
+    </>
   )
 }
